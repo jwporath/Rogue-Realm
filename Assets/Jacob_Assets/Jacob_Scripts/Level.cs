fbfeea1b16e74d1a934ba1f2278bae2c3819a96c
jwporath <jwporath@nic.edu>
2024-03-19 19:55:02 -0700

    Created experimental room generation framework

--- Assets/Jacob_Assets/Jacob_Scripts/Level.cs
+++ Assets/Jacob_Assets/Jacob_Scripts/Level.cs
@@ -5,13 +5,51 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     [SerializeField] private GameObject RoomPrefab;
+    [SerializeField] private GameObject Platform1;
+    [SerializeField] private GameObject Platform2;
+    [SerializeField] private GameObject Platform3;
+    [SerializeField] private GameObject Platform4;
+    [SerializeField] private GameObject Platform5;
+    [SerializeField] private GameObject Platform6;
+
+    private GameObject[,] RoomMap = new GameObject[9, 9];
      
     // Start is called before the first frame update
     void Start()
     {
-        GameObject Room = Instantiate(RoomPrefab, new Vector3(18, 0, 0), Quaternion.identity);
+        // Place starting room in array
+        Component[] startRoom = this.GetComponentsInChildren<Component>();
+        foreach(Component i in startRoom)
+        {
+            if(i.name == "Room")
+            {
+                RoomMap[4, 4] = i.gameObject;
+            }
+        }
+        GenerateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateLevel()
+    {
+        GenerateRoom();
+    }
+
+    GameObject GenerateRoom()
+    {
+        // create room
+        GameObject Room = Instantiate(RoomPrefab, new Vector3(18, 0, 0), Quaternion.identity, this.gameObject.transform);
+
+        // Enable door
         LDoor LeftDoor1 = Room.GetComponentInChildren<LDoor>(true);
         LeftDoor1.gameObject.SetActive(true);
+
+        // Disable NoDoor object
         Component[] LeftNoDoor1 = Room.GetComponentsInChildren<Component>();
         foreach(Component i in LeftNoDoor1)
         {
@@ -20,11 +58,32 @@ public class Level : MonoBehaviour
                 i.gameObject.SetActive(false);
             }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int random = UnityEngine.Random.Range(1, 7);
+        GameObject platform;
+        switch(random)
+        {
+            case 1:
+                platform = Instantiate(Platform1, Room.transform);
+                break;
+            case 2:
+                platform = Instantiate(Platform2, Room.transform);
+                break;
+            case 3:
+                platform = Instantiate(Platform3, Room.transform);
+                break;
+            case 4:
+                platform = Instantiate(Platform4, Room.transform);
+                break;
+            case 5:
+                platform = Instantiate(Platform5, Room.transform);
+                break;
+            case 6:
+                platform = Instantiate(Platform6, Room.transform);
+                break;
+            
+        }
+
+        return Room;
     }
 }
