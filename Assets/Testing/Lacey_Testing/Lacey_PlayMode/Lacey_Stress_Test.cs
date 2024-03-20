0cd9eb2d72b248dadd2f65d6ab9b49111741d356
laceyhunt <hunt3247@vandals.uidaho.edu>
2024-03-20 16:22:48 -0700

    Lacey Tests

--- Assets/Testing/Lacey_Testing/Lacey_PlayMode/Lacey_Stress_Test.cs
+++ Assets/Testing/Lacey_Testing/Lacey_PlayMode/Lacey_Stress_Test.cs
@@ -1,14 +1,14 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class PlayerTest
+public class Lacey_Stress_Test
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void PlayerTestSimplePasses()
+    public void Lacey_Stress_TestSimplePasses()
     {
         // Use the Assert class to test conditions
     }
@@ -16,12 +16,10 @@ public class PlayerTest
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator PlayerTestWithEnumeratorPasses()
+    public IEnumerator Lacey_Stress_TestWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
-        // Assert.
         // Use yield to skip a frame.
         yield return null;
     }
 }
-*/
