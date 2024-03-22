5d46e1bd892ef29596f1aaa29df87e3320867101
jwporath <jwporath@nic.edu>
2024-03-21 17:55:16 -0700

    Level generator is functional with some undesired exception cases.

--- Assets/Jacob_Assets/Jacob_Scripts/Level.cs
+++ Assets/Jacob_Assets/Jacob_Scripts/Level.cs
@@ -14,7 +14,7 @@ public class Level : MonoBehaviour
 
     private Room[,] RoomMap = new Room[9, 9];
      
-    void Awake()
+    void Start()
     {
         // Set all elements in RoomMap to null
         for (int y = 0; y < 9; y++)
@@ -27,14 +27,25 @@ public class Level : MonoBehaviour
 
         // Place starting room in array
         RoomMap[4, 4] = this.GetComponentInChildren<Room>();
+        RoomMap[4, 4].SetX(4);
+        RoomMap[4, 4].SetY(4);
 
         GenerateRooms();
     }
 
     void GenerateRooms()
     {
-        // Create Right default room
-        GameObject room = Instantiate(RoomPrefab, new Vector3(18, 0, 0), Quaternion.identity, this.gameObject.transform);
+        GenerateRoom(5, 4, RoomMap[4, 4]);
+        if(RoomMap[3, 4] == null)
+        {
+            GenerateRoom(3, 4, RoomMap[4, 4]);
+        }        
+    }
+
+    void GenerateRoom(int x, int y, Room Caller)
+    {
+        // Create room
+        GameObject room = Instantiate(RoomPrefab, Caller.transform.position, Quaternion.identity, this.gameObject.transform);
         room.transform.gameObject.tag = "New";
 
         // Add Room to RoomMap
@@ -43,85 +54,142 @@ public class Level : MonoBehaviour
         {
             if (i.tag == "New")
             {
-                RoomMap[5, 4] = i;
-                RoomMap[5, 4].gameObject.transform.tag = "Untagged";
-                RoomMap[5, 4].SetX(5);
-                RoomMap[5, 4].SetY(4);
-                RoomMap[5, 4].EnableLeftDoor();
+                RoomMap[x, y] = i;
+                RoomMap[x, y].gameObject.transform.tag = "Untagged";
+                RoomMap[x, y].SetX(x);
+                RoomMap[x, y].SetY(y);
             }
         }
 
-        // Create Left default room
-        room = Instantiate(RoomPrefab, new Vector3(-18, 0, 0), Quaternion.identity, this.gameObject.transform);
-        room.transform.gameObject.tag = "New";
-
-        // Add Room to RoomMap
-        arr = this.GetComponentsInChildren<Room>();
-        foreach (Room i in arr)
+        // Move to correct position
+        Vector3 vector;
+        if (x > Caller.GetX()) // Move new room to Right
         {
-            if (i.tag == "New")
-            {
-                RoomMap[3, 4] = i;
-                RoomMap[3, 4].gameObject.transform.tag = "Untagged";
-                RoomMap[3, 4].SetX(5);
-                RoomMap[3, 4].SetY(4);
-                RoomMap[3, 4].EnableRightDoor();
-            }
+            vector = Caller.transform.position;
+            vector.x += 18;
+            RoomMap[x, y].transform.position = vector;
+            RoomMap[x, y].EnableLeftDoor();
+        }
+        else if (y > Caller.GetY()) // Move new room to Bottom
+        {
+            vector = Caller.transform.position;
+            vector.y -= 10;
+            RoomMap[x, y].transform.position = vector;
+            RoomMap[x, y].EnableTopDoor();
+        }
+        else if (x < Caller.GetX()) // Move new room to Left
+        {
+            vector = Caller.transform.position;
+            vector.x -= 18;
+            RoomMap[x, y].transform.position = vector;
+            RoomMap[x, y].EnableRightDoor();
+        }
+        else // Move new room to Top
+        {
+            vector = Caller.transform.position;
+            vector.y += 10;
+            RoomMap[x, y].transform.position = vector;
+            RoomMap[x, y].EnableBottomDoor();
         }
 
-        // Add Platforms to Left
+        // Create platforms
         int random = UnityEngine.Random.Range(1, 7);
         switch(random)
         {
             case 1:
-                Instantiate(Platform1, RoomMap[3, 4].transform);
+                Instantiate(Platform1, RoomMap[x, y].transform);
                 break;
             case 2:
-                Instantiate(Platform2, RoomMap[3, 4].transform);
+                Instantiate(Platform2, RoomMap[x, y].transform);
                 break;
             case 3:
-                Instantiate(Platform3, RoomMap[3, 4].transform);
+                Instantiate(Platform3, RoomMap[x, y].transform);
                 break;
             case 4:
-                Instantiate(Platform4, RoomMap[3, 4].transform);
+                Instantiate(Platform4, RoomMap[x, y].transform);
                 break;
             case 5:
-                Instantiate(Platform5, RoomMap[3, 4].transform);
+                Instantiate(Platform5, RoomMap[x, y].transform);
                 break;
             case 6:
-                Instantiate(Platform6, RoomMap[3, 4].transform);
+                Instantiate(Platform6, RoomMap[x, y].transform);
                 break;
             
         }
 
-        // Add Platforms to Right
-        random = UnityEngine.Random.Range(1, 7);
-        switch(random)
+        // Recursive generation calls
+        int Left = UnityEngine.Random.Range(1, 4);
+        int Right = UnityEngine.Random.Range(1, 4);
+        int Top = UnityEngine.Random.Range(1, 4);
+        int Bottom = UnityEngine.Random.Range(1, 4);
+
+        if (Left == 1 && x > 0) // Left
         {
-            case 1:
-                Instantiate(Platform1, RoomMap[5, 4].transform);
-                break;
-            case 2:
-                Instantiate(Platform2, RoomMap[5, 4].transform);
-                break;
-            case 3:
-                Instantiate(Platform3, RoomMap[5, 4].transform);
-                break;
-            case 4:
-                Instantiate(Platform4, RoomMap[5, 4].transform);
-                break;
-            case 5:
-                Instantiate(Platform5, RoomMap[5, 4].transform);
-                break;
-            case 6:
-                Instantiate(Platform6, RoomMap[5, 4].transform);
-                break;
-            
+            if (RoomMap[x - 1, y] == null)
+            {
+                RoomMap[x, y].EnableLeftDoor();
+                GenerateRoom(RoomMap[x, y].GetX() - 1, RoomMap[x, y].GetY(), RoomMap[x, y]);
+            }
+            else
+            {
+                int connect = UnityEngine.Random.Range(1, 3);
+                if (connect == 1)
+                {
+                    RoomMap[x, y].EnableLeftDoor();
+                    RoomMap[x - 1, y].EnableRightDoor();
+                }
+            }
         }
-    }
-
-    void GenerateRoom(int x, int y, Room Caller)
-    {
-
+        if (Right == 1 && x < 8) // Right
+        {
+            if (RoomMap[x + 1, y] == null)
+            {
+                RoomMap[x, y].EnableRightDoor();
+                GenerateRoom(RoomMap[x, y].GetX() + 1, RoomMap[x, y].GetY(), RoomMap[x, y]);
+            }
+            else
+            {
+                int connect = UnityEngine.Random.Range(1, 3);
+                if (connect == 1)
+                {
+                    RoomMap[x, y].EnableRightDoor();
+                    RoomMap[x + 1, y].EnableLeftDoor();
+                }
+            }
+        }
+        if (Top == 1 && y > 0) // Top
+        {
+            if (RoomMap[x, y - 1] == null)
+            {
+                RoomMap[x, y].EnableTopDoor();
+                GenerateRoom(RoomMap[x, y].GetX(), RoomMap[x, y].GetY() - 1, RoomMap[x, y]);
+            }
+            else
+            {
+                int connect = UnityEngine.Random.Range(1, 3);
+                if (connect == 1)
+                {
+                    RoomMap[x, y].EnableTopDoor();
+                    RoomMap[x, y - 1].EnableBottomDoor();
+                }
+            }
+        }
+        if (Bottom == 1 && y < 8) // Bottom
+        {
+            if (RoomMap[x, y + 1] == null)
+            {
+                RoomMap[x, y].EnableBottomDoor();
+                GenerateRoom(RoomMap[x, y].GetX(), RoomMap[x, y].GetY() + 1, RoomMap[x, y]);
+            }
+            else
+            {
+                int connect = UnityEngine.Random.Range(1, 3);
+                if (connect == 1)
+                {
+                    RoomMap[x, y].EnableBottomDoor();
+                    RoomMap[x, y + 1].EnableTopDoor();
+                }
+            }
+        }
     }
 }
