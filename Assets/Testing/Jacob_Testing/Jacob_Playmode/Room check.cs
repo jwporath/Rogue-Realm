8a34e5653a4c74ab2c76202b32f95a035b4c2b6d
jwporath <jacob_porath@outlook.com>
2024-03-25 15:08:48 -0700

    Added basic boundary tests

--- Assets/Testing/Jacob_Testing/Jacob_Playmode/Room check.cs
+++ Assets/Testing/Jacob_Testing/Jacob_Playmode/Room check.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEditor.VersionControl;
 using UnityEngine;
@@ -8,11 +9,30 @@ using UnityEngine.TestTools;
 
 public class Roomcheck
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void RoomExists()
+    [UnitySetUp]
+    public IEnumerator SetUpScene()
     {
-        SceneManager.LoadScene("Jacob_Gen_Level");
+        // Load the test scene
+        yield return SceneManager.LoadSceneAsync("Jacob_Gen_Level");
+    }
+
+    // A Test behaves as an ordinary method
+    [UnityTest]
+    public IEnumerator LevelExists()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        GameObject l1 = GameObject.Find("Level");
+
+        // Assert level exists
+        Assert.IsTrue(l1);
+    }
+
+    [UnityTest]
+    public IEnumerator RoomExists()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
         GameObject l1 = GameObject.Find("Level");
         Level l2 = l1.GetComponent<Level>();
 
@@ -20,5 +40,22 @@ public class Roomcheck
 
         // Assert room exists
         Assert.IsTrue(r);
+    }
+
+    [UnityTest] // WIP, Logic errors here
+    public IEnumerator NoRoomOverlap()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        bool valid = true;
+        Room[] r = GameObject.FindObjectsOfType<Room>();
+
+        if (r.Length != r.Distinct().Count())
+        {
+            valid = false;
+        }
+
+        // Assert room exists
+        Assert.IsTrue(valid);
     }
 }
