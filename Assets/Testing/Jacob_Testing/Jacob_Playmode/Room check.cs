3e08c333ecaa14dd9c3317bfd471128b2b6a26c1
jwporath <jwporath@nic.edu>
2024-04-15 12:37:50 -0700

    expanded level generator tests

--- Assets/Testing/Jacob_Testing/Jacob_Playmode/Room check.cs
+++ Assets/Testing/Jacob_Testing/Jacob_Playmode/Room check.cs
@@ -58,4 +58,68 @@ public class Roomcheck
         // Assert room exists
         Assert.IsTrue(valid);
     }
+
+    [UnityTest]
+    public IEnumerator BossRoomExists()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        BossRoom r = GameObject.FindObjectOfType<BossRoom>();
+
+        // Assert room exists
+        Assert.IsTrue(r);
+    }
+
+    [UnityTest]
+    public IEnumerator GeneratesEnemies()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        enemyBehavior e = GameObject.FindObjectOfType<enemyBehavior>();
+
+        // Assert room exists
+        Assert.IsTrue(e);
+    }
+
+    [UnityTest]
+    public IEnumerator GeneratesItems()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        GameObject[] arr = GameObject.FindGameObjectsWithTag("ItemSpawn");
+
+        bool hasItem = false;
+        foreach(GameObject i in arr)
+        {
+            if (i.GetComponentInChildren<GameObject>() != null)
+                hasItem = true;
+        }
+
+        // Assert room exists
+        Assert.IsTrue(hasItem);
+    }
+
+    [UnityTest]
+    public IEnumerator GeneratesPlatforms()
+    {
+        // wait for scene to call start()
+        yield return new WaitForSeconds(1f);
+        Room[] RoomArr = GameObject.FindObjectsOfType<Room>();
+        List<Component> lplatforms = new List<Component>();
+
+        foreach(Room i in RoomArr)
+        {
+            Component[] objects = i.gameObject.GetComponentsInChildren<Component>();
+            foreach(Component k in objects)
+            {
+                if (k.name.Contains("Platform"))
+                {
+                    lplatforms.Add(k);
+                    break;
+                }
+            }
+        }
+        
+        Assert.IsTrue(lplatforms.Count() == RoomArr.Length - 1);
+    }
 }
