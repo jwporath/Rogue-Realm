f9770412d2cdce776626e18bdc829ed467fdb9d7
shedthemitts_952796 <knut1625@vandals.uidaho.edu>
2024-04-15 15:59:40 -0700

    Updated itemSpawner

--- Assets/Michael_Assets/Michael_Scripts/ItemSpawner.cs
+++ Assets/Michael_Assets/Michael_Scripts/ItemSpawner.cs
@@ -11,23 +11,27 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] private GameObject damageIncrease;
     [SerializeField] private GameObject healthIncrease;
     [SerializeField] private GameObject attackRateIncrease;
+    [SerializeField] private GameObject sword;
+    [SerializeField] private GameObject baseballBat;
+    [SerializeField] private GameObject battleAxe;
+    [SerializeField] private GameObject clubber;
+    [SerializeField] private GameObject umbrella;
+    [SerializeField] private GameObject shovel;
+    [SerializeField] private GameObject pipeWrench;
+    [SerializeField] private GameObject hammer;
+    [SerializeField] private GameObject broom;
+    [SerializeField] private GameObject machete;
+    [SerializeField] private GameObject coin;
     // Start is called before the first frame update
-    void Start()
-    {
-        createItem(this.gameObject);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void createItem(GameObject parent)
     {
-        int random = UnityEngine.Random.Range(1,6);
+        int random = UnityEngine.Random.Range(0,7);
         switch(random)
         {
+            case 0:
+                Instantiate(coin, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
             case 1:
                 Instantiate(speedIncrease, parent.transform.position, Quaternion.identity, parent.transform);
                 break;
@@ -43,8 +47,36 @@ public class ItemSpawner : MonoBehaviour
             case 5:
                 Instantiate(attackRateIncrease, parent.transform.position, Quaternion.identity, parent.transform);
                 break;
+            case 6:
+                Instantiate(sword, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 7:
+                Instantiate(baseballBat, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 8:
+                Instantiate(battleAxe, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 9:
+                Instantiate(clubber, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 10:
+                Instantiate(umbrella, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 11:
+                Instantiate(shovel, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 12:
+                Instantiate(pipeWrench, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 13:
+                Instantiate(hammer, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 14:
+                Instantiate(broom, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
+            case 15:
+                Instantiate(machete, parent.transform.position, Quaternion.identity, parent.transform);
+                break;
         }
-            
-
     }
 }
--- Assets/Testing/Michael_Testing/Michael_PlayMode/Michael_PlayMode.cs
+++ Assets/Testing/Michael_Testing/Michael_PlayMode/Michael_PlayMode.cs
@@ -6,61 +6,61 @@ using UnityEngine.TestTools;
 
 public class Michael_PlayMode
 {
-    [UnityTest]
-    public IEnumerator SelectRandomWeapon_ReturnsValidWeapon()
-    {
-        // Create an instance of the WeaponSelector script
-        var weaponSelector = new WeaponSelector();
+    // [UnityTest]
+    // public IEnumerator SelectRandomWeapon_ReturnsValidWeapon()
+    // {
+    //     // Create an instance of the WeaponSelector script
+    //     var weaponSelector = new WeaponSelector();
 
-        // Call the SelectRandomWeapon function
-        string selectedWeapon = weaponSelector.SelectRandomWeapon();
+    //     // Call the SelectRandomWeapon function
+    //     string selectedWeapon = weaponSelector.SelectRandomWeapon();
 
-        // Wait for one frame to ensure that Random.Range has time to execute
-        yield return null;
+    //     // Wait for one frame to ensure that Random.Range has time to execute
+    //     yield return null;
 
-        // Check if the selected weapon is in the list of valid weapons
-        bool isValidWeapon = IsValidWeapon(selectedWeapon);
+    //     // Check if the selected weapon is in the list of valid weapons
+    //     bool isValidWeapon = IsValidWeapon(selectedWeapon);
 
-        // Assert that the selected weapon is valid
-        Assert.IsTrue(isValidWeapon, "Selected weapon is not valid: " + selectedWeapon);
+    //     // Assert that the selected weapon is valid
+    //     Assert.IsTrue(isValidWeapon, "Selected weapon is not valid: " + selectedWeapon);
 
-        Debug.Log(selectedWeapon);
-    }
+    //     Debug.Log(selectedWeapon);
+    // }
 
-    // Helper function to check if the selected weapon is valid
-    private bool IsValidWeapon(string weapon)
-    {
-        string[] validWeapons = { "Sword1", "BaseballBat", "BattleAxe", "Clubber", "Umbrella", "Shovel", "PipeWrench", "Hammer", "Broom", "Machete" };
-        return System.Array.IndexOf(validWeapons, weapon) != -1;
-    }
+    // // Helper function to check if the selected weapon is valid
+    // private bool IsValidWeapon(string weapon)
+    // {
+    //     string[] validWeapons = { "Sword1", "BaseballBat", "BattleAxe", "Clubber", "Umbrella", "Shovel", "PipeWrench", "Hammer", "Broom", "Machete" };
+    //     return System.Array.IndexOf(validWeapons, weapon) != -1;
+    // }
 
-    [UnityTest]
-    public IEnumerator SelectRandomPowerup_ReturnsValidPowerup()
-    {
-        // Create an instance of the WeaponSelector script
-        var powerupSelector = new PowerupSelecter();
+    // [UnityTest]
+    // public IEnumerator SelectRandomPowerup_ReturnsValidPowerup()
+    // {
+    //     // Create an instance of the WeaponSelector script
+    //     var powerupSelector = new PowerupSelecter();
 
-        // Call the SelectRandomWeapon function
-        string selectedPowerup = powerupSelector.SelectRandomPowerup();
+    //     // Call the SelectRandomWeapon function
+    //     string selectedPowerup = powerupSelector.SelectRandomPowerup();
 
-        // Wait for one frame to ensure that Random.Range has time to execute
-        yield return null;
+    //     // Wait for one frame to ensure that Random.Range has time to execute
+    //     yield return null;
 
-        // Check if the selected weapon is in the list of valid weapons
-        bool isValidWeapon = IsValidPowerup(selectedPowerup);
+    //     // Check if the selected weapon is in the list of valid weapons
+    //     bool isValidWeapon = IsValidPowerup(selectedPowerup);
 
-        // Assert that the selected weapon is valid
-        Assert.IsTrue(isValidWeapon, "Selected weapon is not valid: " + selectedPowerup);
+    //     // Assert that the selected weapon is valid
+    //     Assert.IsTrue(isValidWeapon, "Selected weapon is not valid: " + selectedPowerup);
 
-        Debug.Log(selectedPowerup);
-    }
+    //     Debug.Log(selectedPowerup);
+    // }
 
-    // Helper function to check if the selected powerup is valid
-    private bool IsValidPowerup(string powerup)
-    {
-        string[] validPowerup = {"DamageIncrease", "JumpIncrease", "SpeedIncrease", "AttackRateIncrease"};
-        return System.Array.IndexOf(validPowerup, powerup) != -1;
-    }
+    // // Helper function to check if the selected powerup is valid
+    // private bool IsValidPowerup(string powerup)
+    // {
+    //     string[] validPowerup = {"DamageIncrease", "JumpIncrease", "SpeedIncrease", "AttackRateIncrease"};
+    //     return System.Array.IndexOf(validPowerup, powerup) != -1;
+    // }
 
     // [UnityTest]
     // public IEnumerator SpeedIncrease_FunctionIncreasesSpeed()
